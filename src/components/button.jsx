b7c37aa70180401ca707dc3df05db35166ea1a8d
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-02-27 10:03:24 +0530

    done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,12 @@
 // write button card here
+import React from "react";
+
+const Button = () =>{
+    return(
+        <button>
+            View Product
+        </button>
+    )
+}
+
+export default Button;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,30 @@
 // write product card here
+import React from "react";
+import Button from "./button";
+
+const Productcard = () =>{
+    const productImage = 'Untitled.png';
+    const productName = 'ASOIAF Book';
+    const price = '$49';
+   
+    
+
+    return(
+        
+        <div className = 'product-card-details'>
+          
+            <img src = {productImage} alt = {productImage} className="image"/>
+            <div className="product-details">
+                <h1>{productName}</h1>
+                <h2>{price}</h2>
+                <Button/>
+                
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default Productcard;
+
