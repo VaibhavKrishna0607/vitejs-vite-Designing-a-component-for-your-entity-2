353a5044d6a94f9316b4704377f77516210758be
VaibhavKrishna0607 <vaibhavkrishna0607@gmail.com>
2025-02-27 10:05:11 +0530

    dracrays

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Button from "./button";
 
 const Productcard = () =>{
-    const productImage = 'Untitled.png';
+    const productImage = 'Untitled.jpg';
     const productName = 'ASOIAF Book';
     const price = '$49';
    
